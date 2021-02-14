95085714bde3af5422255cc436f1f676b4cb499d
Sarah Di <sarahdi@andrew.cmu.edu>
2021-02-14 02:26:46 -0500

    images

--- src/background.js
+++ src/background.js
@@ -1,6 +1,7 @@
 // background.js
 
 // called when the user clicks on the browser action.
+/*
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -8,12 +9,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
-
+*/
 // This block is new!
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
+      window.open(request.url,'_blank');
+      //chrome.tabs.create({"url": request.url});
     }
   }
 );
--- src/content.js
+++ src/content.js
@@ -18,7 +18,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/cal/gi, "butter"); // replaces "cal," "Cal", etc. with "butt"
+            var replacedText = text.replace(/cal/gi, "butt"); // replaces "cal," "Cal", etc. with "butt"
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
@@ -27,6 +27,18 @@ for (var i = 0; i < elements.length; i++) {
     }
 }
 
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
+
+/*
+document.addEventListener("click", function() {
+  var firstHref = "https://www.youtube.com/watch?v=n7zMZ-7Dk8E&list=PLfAUOI05WS6UxAEVmhMe6U8SOYVyNCxNy";
+  chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+}, false);
+*/
+/*
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
@@ -38,3 +50,4 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+*/
